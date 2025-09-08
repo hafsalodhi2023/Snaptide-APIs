6fb343b80934aa919a8ed3c94519bd9fcd9bc796
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2025-09-08 17:54:19 +0500

    Fix: remove unnecessary _id field from user profile retrieval

--- controllers/users/get-profile.controller.js
+++ controllers/users/get-profile.controller.js
@@ -15,7 +15,7 @@ const getProfile = async (req, res) => {
     const { id } = decodedAccessToken;
 
     const user = await User.findById(id).select(
-      "-password -__v -createdAt -updatedAt -_id -provider -isVerified -googleId"
+      "-password -__v -createdAt -updatedAt -provider -isVerified -googleId"
     );
 
     if (!user) {
