456dbd77957d10f41d33b4d97ccd1d9dfb14a98f
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-09-08 21:04:58 +0500

    add code in refresh-controller

--- controllers/auths/refresh.controller.js
+++ controllers/auths/refresh.controller.js
@@ -13,13 +13,35 @@ const {
 } = require("../../utils/token.util");
 const { setRefreshCookie } = require("../../utils/refreshCookie.util");
 
+// const refresh = async (req, res) => {
+//   try {
+//     const refreshToken = req.cookies.refreshToken;
+//     if (!refreshToken) return res.status(401).json({ message: "Unauthorized" });
+
+//     const decoded = verifyRefreshToken(refreshToken);
+
+//     const user = await User.findById(decoded.id);
+//     if (!user) return res.sendStatus(403);
+
+//     const accessToken = signAccessToken(user);
+//     const newRefreshToken = signRefreshToken(user);
+
+//     setRefreshCookie(res, newRefreshToken);
+//     return res.status(200).json({ accessToken });
+//   } catch (error) {
+//     debug(error);
+//     return res.status(403).json({ message: "Forbidden" });
+//   }
+// };
+
 const refresh = async (req, res) => {
   try {
+    console.log("🍪 Refresh token cookie:", req.cookies.refreshToken);
+
     const refreshToken = req.cookies.refreshToken;
     if (!refreshToken) return res.status(401).json({ message: "Unauthorized" });
 
     const decoded = verifyRefreshToken(refreshToken);
-
     const user = await User.findById(decoded.id);
     if (!user) return res.sendStatus(403);
 
@@ -29,7 +51,7 @@ const refresh = async (req, res) => {
     setRefreshCookie(res, newRefreshToken);
     return res.status(200).json({ accessToken });
   } catch (error) {
-    debug(error);
+    console.log("❌ Refresh controller error:", error.message);
     return res.status(403).json({ message: "Forbidden" });
   }
 };
