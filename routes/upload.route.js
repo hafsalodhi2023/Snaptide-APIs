7da7ee6f4e6f43e8903e7c2c2d4bb99b1af393f7
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2025-09-17 15:31:27 +0500

    fix: change profile image upload route from POST to PUT for better RESTful practices

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -4,6 +4,6 @@ const { uploadSingleImage } = require("../middlewares/multer.middleware");
 const uploadProfileImage = require("../controllers/uploads/profile-upload.controller");
 
 // Upload profile avatar
-router.post("/profile", uploadSingleImage("avatar"), uploadProfileImage);
+router.put("/profile", uploadSingleImage("avatar"), uploadProfileImage);
 
 module.exports = router;
