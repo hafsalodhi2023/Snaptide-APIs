0ae1103f47439b8eab3213574b5c5777a8722ac7
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2025-09-23 21:39:07 +0500

    update CORS configuration to allow localhost for development

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
